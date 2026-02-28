70a4310c01af8274c01597c72f15c3cca4e42f32
mimi-star12 <mimivixx12@gmail.com>
2026-02-28 11:59:16 +0300

    gitignore added?

---
+++ lab2.numpy/main.py
@@ -0,0 +1,28 @@
+import os
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+def create_vector():
+    """
+    """
+    a=np.arange(10)
+    return a
+    
+
+def create_matrix():
+    """
+    """
+    a=np.random.rand(5,5)
+    return a
+
+
+def reshape_vector(vec):
+    """
+    """
+    return
+
+
+
+print(create_vector(), create_matrix())
